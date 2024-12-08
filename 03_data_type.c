d698024e49ccbc2d7392505e94441adb590a42b5
Shashank Singh <singshashank7@gmail.com>
2024-12-08 22:39:47 +0530

    Create 03_data_type.c

---
+++ 03_data_type.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    // int a;
+    // a = 1;
+    int a = 1;
+
+    //float b = 1.4;
+    float b;
+    b = 1.4;
+
+    // char c = 'a';
+    char c;
+    c = 'a';
+    printf("The first output is %d\n", a);
+    printf("The second output is %f\n", b);
+    printf("The third output is %c", c);
+    return 0;
+
+    }
