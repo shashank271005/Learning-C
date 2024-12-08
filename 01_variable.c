f725314d92f77bb8ea65a5f33976c1fe28aa62dd
Shashank Singh <singshashank7@gmail.com>
2024-12-08 22:36:41 +0530

    Create 01_variable.c

---
+++ 01_variable.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int a; //variable declaration
+    a=6;   //variable initialization
+    printf("The output of this program is %d",a);
+    printf("\nHello World");
+
+    return 0;
+    }
