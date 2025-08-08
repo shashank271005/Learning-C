bab8e9231254bd477468429b1caa9bf0fa638b32
Shashank Singh <singshashank7@gmail.com>
2025-08-08 12:02:40 +0530

    Update problem37.c

--- problem37.c
+++ problem37.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
+void change_to_thirty_times(int);
 
-int sum_natural(int);
-
-int sum_natural(int n){
-    if(n==1){
-        return 1;
-    }
-    // sum_natural(n) = 1 + 2 + 3 + 4 + 5 + ... n-1 + n;
-    // sum_natural(n) = sum(n-1) + n;
-    return sum_natural(n-1) + n;
-
+void change_to_thirty_times(int a){
+    a = a * 10;
 }
+
 int main(){
-    printf("The sum of first 5 natural numbers is %d", sum_natural(5));
+    int x = 45;
+    printf("The value of x is %d\n", x);
+    change_to_thirty_times(x);
+    printf("The value of x is %d\n", x);
+
     return 0;
 }
